285136ac1e58f8c9cbca2599acffc11ce7cce9af
berges99 <daviid.bergees@gmail.com>
2024-07-07 09:49:48 +0200

    pydantic Field interface changed after v2.

--- timbal/types.py
+++ timbal/types.py
@@ -15,17 +15,20 @@ def Input(
     """
     Input is similar to pydantic.Field, but doesn't require a default value to be the first argument.
     Original source: https://github.com/replicate/cog/blob/main/python/cog/types.py
+    Parameters are kept the same to maintain compatibility with cog.
 
     If default is not provided, the field is required.
     If default is explicitly set to None, then the field is optional.
     """
-    return Field(
-        default,
-        description = description,
-        ge = ge,
-        le = le,
-        min_length = min_length,
-        max_length = max_length,
-        regex = regex,
-        choices = choices,
-    )
+    field_info = {
+        "description": description,
+        "ge": ge,
+        "le": le,
+        "min_length": min_length,
+        "max_length": max_length,
+        "pattern": regex,
+    }
+    # Choices is not implemented in pydantic Field. This will be added in json_schema_extra.
+    if choices is not None:
+        field_info["choices"] = choices
+    return Field(default, **field_info)
