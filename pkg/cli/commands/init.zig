7c7660b703a21b50d5c463b66be3cdeb0dc01aac
berges99 <daviid.bergees@gmail.com>
2025-02-21 16:48:01 +0100

    Init python project with uv and installed timbal dependency

--- pkg/cli/commands/init.zig
+++ pkg/cli/commands/init.zig
@@ -4,12 +4,14 @@ const fs = std.fs;
 
 // Embed template files into the binary.
 const dockerignore = @embedFile("../init-templates/.dockerignore");
+const pyproject_toml = @embedFile("../init-templates/pyproject.toml");
 const timbal_yaml = @embedFile("../init-templates/timbal.yaml");
 const flow_py = @embedFile("../init-templates/flow.py");
 
 
 pub fn run(target_path: []const u8) !void {
     const cwd = fs.cwd();
+    const allocator = std.heap.page_allocator;
 
     const use_current_dir = std.mem.eql(u8, target_path, ".");
 
@@ -22,11 +24,17 @@ pub fn run(target_path: []const u8) !void {
     else 
         try cwd.openDir(target_path, .{});
 
-    const path = try app_dir.realpathAlloc(std.heap.page_allocator, ".");
+    const path = try app_dir.realpathAlloc(allocator, ".");
     std.debug.print("Initializing timbal project in {s}\n", .{path});
+
+    // Change the name of the app in the pyproject.toml file.
+    const app_name = std.fs.path.basename(path);
+    const pyproject_toml_replaced = try std.mem.replaceOwned(
+        u8, allocator, pyproject_toml, "{{app_name}}", app_name);
 
     const init_templates = [_]struct { content: []const u8, dist: []const u8 } {
         .{ .content = dockerignore, .dist = ".dockerignore" },
+        .{ .content = pyproject_toml_replaced, .dist = "pyproject.toml" },
         .{ .content = timbal_yaml, .dist = "timbal.yaml" },
         .{ .content = flow_py, .dist = "flow.py" },
     };
@@ -35,5 +43,26 @@ pub fn run(target_path: []const u8) !void {
         const dist_file = try app_dir.createFile(init_template.dist, .{});
         defer dist_file.close();
         try dist_file.writeAll(init_template.content);
+    }
+
+    try uv_sync(allocator, target_path);
+}
+
+
+fn uv_sync(allocator: std.mem.Allocator, target_path: []const u8) !void {
+    const uv_args = [_][]const u8{
+        "uv", "sync",
+        "--python", "3.12",
+        "--python-preference", "managed",
+    };
+
+    const uv_result = try std.process.Child.run(.{
+        .allocator = allocator,
+        .argv = &uv_args,
+        .cwd = target_path,
+    });
+
+    if (uv_result.stderr.len > 0) {
+        std.debug.print("uv sync warning: {s}\n", .{uv_result.stderr});
     }
 }
