c20ed529132ffb9996983e85749a0e7f3314b840
berges99 <daviid.bergees@gmail.com>
2025-03-14 10:24:38 +0100

    New CLI flow.py init template.

--- cli/init-templates/flow.py
+++ cli/init-templates/flow.py
@@ -1,13 +1,37 @@
+import asyncio
+from datetime import datetime
+
+from timbal import Agent
+from timbal.state import RunContext
+from timbal.state.savers import TimbalPlatformSaver
 
 
-from timbal import Flow
-from timbal.state.savers import InMemorySaver
+def get_datetime() -> str:
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
-flow = (
-    Flow()
-    .add_llm(model="gpt-4o-mini", memory_id="llm")
-    .set_data_map("llm.prompt", "prompt")
-    .set_output("response", "llm.return")
-    .compile(state_saver=InMemorySaver())
-)
+flow = Agent(
+    model="gpt-4o-mini",
+    tools=[get_datetime],
+).compile(state_saver=TimbalPlatformSaver())
+
+
+async def main():
+    run_context = RunContext()
+    while True:
+        prompt = input("User: ")
+        if prompt == "q":
+            break
+        flow_output_event = await flow.complete(context=run_context, prompt=prompt)
+        print(f"Agent: {flow_output_event.output}")
+        run_context = RunContext(parent_id=flow_output_event.run_id)
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        # Catch any Ctrl+C that wasn't caught in main()
+        print("\nGoodbye!")
+    finally:
+        print("Goodbye!")
