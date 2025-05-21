70c13339d96cfaa350bd45e4661bb45a2d2f2a50
berges99 <daviid.bergees@gmail.com>
2025-05-21 10:29:50 +0200

    Validating evals files

--- python/timbal/evals/__main__.py
+++ python/timbal/evals/__main__.py
@@ -4,8 +4,10 @@ import json
 import os
 import sys
 from pathlib import Path
+from typing import Any
 
 import structlog
+import yaml
 from dotenv import find_dotenv, load_dotenv
 
 from .. import __version__
@@ -17,17 +19,98 @@ from ..server.utils import ModuleSpec, load_module
 logger = structlog.get_logger("timbal.evals")
 
 
-def discover_evals(evals_path: Path) -> list:
-    evals_files = []
-    if evals_path.is_dir():
-        for file in evals_path.rglob("eval*.yaml"):
-            evals_files.append(file)
-    else:
-        if not evals_path.name.endswith(".yaml"):
-            raise ValueError(f"Invalid evals path: {evals_path}")
-        evals_files.append(evals_path)
+from pydantic import BaseModel, field_validator
 
-    return evals_files
+from ..types.message import Message
+from ..types.file import File
+
+from collections.abc import Callable
+
+
+Validator = Callable[[Message], bool]
+
+
+class Turn(BaseModel):
+    """"""
+
+    input: Message
+    steps: str | dict[str, Any] | None = None
+    output: Message | list[Validator]
+
+    @field_validator("input", mode="before")
+    def validate_input(cls, v):
+        if isinstance(v, str):
+            return Message.validate(v)
+        elif isinstance(v, dict):
+            content = []
+            if "files" in v:
+                if isinstance(v["files"], list):
+                    for file in v["files"]:
+                        content.append(File.validate(file))
+                else:
+                    raise ValueError("files must be a list")
+            elif "text" in v:
+                content.append(v["text"])
+            return Message.validate(content)
+        raise ValueError("input must be a str or dict")
+    
+    @field_validator("output", mode="before")
+    def validate_output(cls, v):
+        
+        if isinstance(v, str):
+            return Message.validate(v)
+        elif isinstance(v, dict):
+            validators = []
+            for validator, ref in v.items():
+                if validator == "contains":
+                    if not isinstance(ref, list):
+                        raise ValueError("contains must be a list")
+                    validators.append(lambda output: all([x in output for x in ref]))
+                # TODO
+                else:
+                    logger.error("unknown_validator", validator=validator)
+
+            return validators
+
+        raise ValueError("output must be a fixed string or a dict of validators")
+
+
+class Test(BaseModel):
+    """"""
+
+    name: str
+    description: str | None = None
+    turns: list[Turn]
+
+
+class Config(BaseModel):
+    """"""
+
+    tests: list[Test]
+
+
+def discover_files(path: Path) -> list[Path]:
+    """"""
+    files = []
+    if path.is_dir():
+        for file in path.rglob("eval*.yaml"):
+            files.append(file)
+    else:
+        if not path.name.endswith(".yaml"):
+            raise ValueError(f"Invalid evals path: {path}")
+        files.append(path)
+
+    return files
+
+
+def eval_file(path: Path) -> Any:
+    """"""
+    with open(path, "r") as f:
+        config = yaml.safe_load(f)
+    if isinstance(config, list):
+        config = {"tests": config}
+    config = Config.model_validate(config)
+    print(config)
 
 
 if __name__ == "__main__":
@@ -90,4 +173,8 @@ if __name__ == "__main__":
     load_dotenv(override=True)
     setup_logging()
 
-    discover_evals(evals_path)
+    files = discover_files(evals_path)
+    print(files)
+
+    for file in files:
+        eval_file(file)
