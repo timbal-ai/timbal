aeed537e36acace1dacbccc7e9ac1abe9d5dbd01
berges99 <daviid.bergees@gmail.com>
2025-10-08 13:47:00 +0200

    refactor: upgrade command for timbal CLI

--- cli/main.zig
+++ cli/main.zig
@@ -2,46 +2,38 @@ const std = @import("std");
 
 const init_cmd = @import("commands/init.zig");
 const build_cmd = @import("commands/build.zig");
-const run_cmd = @import("commands/run.zig");
 const push_cmd = @import("commands/push.zig");
-
+const upgrade_cmd = @import("commands/upgrade.zig");
 
 // Embedded version.
 const timbal_version = @import("version.zig").timbal_version;
 
-
 fn printUsage() !void {
     const stderr = std.io.getStdErr().writer();
-    try stderr.writeAll(
-        "A strongly opinionated framework for building and orchestrating agentic AI applications.\n" ++
+    try stderr.writeAll("Simple, battle-tested framework for building reliable AI applications.\n" ++
         "\n" ++
         "\x1b[1;32mUsage: \x1b[1;36mtimbal \x1b[0;36m[OPTIONS] <COMMAND>\n" ++
         "\n" ++
         "\x1b[1;32mCommands:\n" ++
-        "    \x1b[1;36minit  \x1b[0mInitialize a new application\n" ++
-        "    \x1b[1;36mbuild \x1b[0mBuild the application into a container\n" ++
-        "    \x1b[1;36mrun   \x1b[0mRun a command inside the built container\n" ++
-        "    \x1b[1;36mserve \x1b[0mBuild and Run the application at once\n" ++
-        "    \x1b[1;36mlogin \x1b[0mLogin to the Timbal Platform\n" ++
-        "    \x1b[1;36mpush  \x1b[0mPush an application to the Timbal Platform\n" ++
-        "    \x1b[1;36mhelp  \x1b[0mDisplay this help message\n" ++
+        "    \x1b[1;36minit    \x1b[0mInitialize a new application\n" ++
+        "    \x1b[1;36mbuild   \x1b[0mBuild the application into a container\n" ++
+        "    \x1b[1;36mpush    \x1b[0mPush an application to the Timbal Platform\n" ++
+        "    \x1b[1;36mupgrade \x1b[0mUpgrade timbal to the latest version\n" ++
+        "    \x1b[1;36mhelp    \x1b[0mDisplay this help message\n" ++
         "\n" ++
         "\x1b[1;32mGlobal options:\n" ++
         "    \x1b[1;36m-q\x1b[0m, \x1b[1;36m--quiet      \x1b[0mDo not print any output\n" ++
         "    \x1b[1;36m-v\x1b[0m, \x1b[1;36m--verbose\x1b[0;36m... \x1b[0mUse verbose output\n" ++
         "    \x1b[1;36m-h\x1b[0m, \x1b[1;36m--help       \x1b[0mDisplay the concise help for this command\n" ++
         "    \x1b[1;36m-V\x1b[0m, \x1b[1;36m--version    \x1b[0mDisplay the timbal version\n" ++
-        "\n"
-    );
+        "\n");
 }
-
 
 fn printUsageWithError(err: []const u8) !void {
     const stderr = std.io.getStdErr().writer();
     try stderr.print("{s}\n\n", .{err});
     try printUsage();
 }
-
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -62,10 +54,10 @@ pub fn main() !void {
         try init_cmd.run(allocator, args[2..]);
     } else if (std.mem.eql(u8, action, "build")) {
         try build_cmd.run(allocator, args[2..]);
-    } else if (std.mem.eql(u8, action, "run")) {
-        try run_cmd.run(allocator, args[2..]);
     } else if (std.mem.eql(u8, action, "push")) {
         try push_cmd.run(allocator, args[2..]);
+    } else if (std.mem.eql(u8, action, "upgrade")) {
+        try upgrade_cmd.run(allocator, args[2..]);
     } else if (std.mem.eql(u8, action, "help")) {
         try printUsage();
     } else if (std.mem.eql(u8, action, "-V") or std.mem.eql(u8, action, "--version")) {
