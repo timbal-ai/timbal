4b9ff54a536fb4023ad05fefddae5d806d554d99
berges99 <daviid.bergees@gmail.com>
2026-03-10 11:58:26 +0100

    Only printing last event when nos treaming in codegen test

--- python/timbal/codegen/test.py
+++ python/timbal/codegen/test.py
@@ -19,10 +19,14 @@ async def run_test(
     if run_context is not None:
         set_run_context(run_context)
 
+    output_event = None
     runnable = import_spec.load()
     async for event in runnable(**params):
         event_dict = event.model_dump()
-        if event.type in ("START", "OUTPUT"):
+        if event.type == "OUTPUT":
+            output_event = event_dict
+        if stream:
             print(event_dict)
-        elif event.type in ("DELTA", "CHUNK") and stream:
-            print(event_dict)
+
+    if not stream:
+        print(output_event)
